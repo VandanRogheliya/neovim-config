8c65fa69c2e488eaa748ed35af7ca50ca4c65f94
Vandan Rogheliya <rogheliavandan@outlook.com>
2024-07-21 17:07:18 +0530

    add harpoon

---
+++ lua/theprimeagen/lazy/harpoon.lua
@@ -0,0 +1,27 @@
+return {
+    "ThePrimeagen/harpoon",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+        local harpoon_ui = require("harpoon.ui")
+        local harpoon_mark = require("harpoon.mark")
+
+        vim.keymap.set("n", "<leader>h", function() harpoon_mark.add_file() end)
+        vim.keymap.set("n", "<C-e>", function() harpoon_ui.toggle_quick_menu() end)
+
+        vim.keymap.set("n", "<C-h>", function() harpoon_ui.nav_file(1) end)
+        vim.keymap.set("n", "<C-t>", function() harpoon_ui.nav_file(2) end)
+        vim.keymap.set("n", "<C-n>", function() harpoon_ui.nav_file(3) end)
+        vim.keymap.set("n", "<C-s>", function() harpoon_ui.nav_file(4) end)
+
+        vim.keymap.set("n", "<leader><C-h>", function() harpoon_mark.set_mark(1) end)
+        vim.keymap.set("n", "<leader><C-t>", function() harpoon_mark.set_mark(2) end)
+        vim.keymap.set("n", "<leader><C-n>", function() harpoon_mark.set_mark(3) end)
+        vim.keymap.set("n", "<leader><C-s>", function() harpoon_mark.set_mark(4) end)
+    end
+
+
+}
--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -8,9 +8,8 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
-
         local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
@@ -25,6 +24,10 @@ return {
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>px", builtin.resume, {
+            noremap = true,
+            silent = true,
+            desc = "Resume",
+        })
     end
 }
-
